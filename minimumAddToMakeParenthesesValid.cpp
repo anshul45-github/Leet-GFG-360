1be37ec72c1ad17a47eeabf81770daac6867efc2
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2024-08-15 11:10:17 +0530

    Create minimumAddToMakeParenthesesValid.cpp

---
+++ minimumAddToMakeParenthesesValid.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<char> st;
+        for(int i = 0; i < s.size(); i++) {
+            if(s[i] == '(')
+                st.push(s[i]);
+            else if(s[i] == ')' && st.empty())
+                st.push(s[i]);
+            else if(s[i] == ')' && st.top() == '(')
+                st.pop();
+            else if(s[i] == ')')
+                st.push(s[i]);
+        }
+        return st.size();
+    }
+};
