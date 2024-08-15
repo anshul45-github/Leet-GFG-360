ba4362add4a27b9b083af4493e2a6a14609142f2
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2024-08-15 11:11:06 +0530

    Create countAndSay.cpp

---
+++ countAndSay.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        if(n == 1)
+            return "1";
+        string s = countAndSay(n - 1);
+        string a = "";
+        for(int i = 0; i < s.size();) {
+            char ch = s[i];
+            int count = 0;
+            while(i < s.size() && s[i] == ch) {
+                i++;
+                count++;
+            }
+            a = a + to_string(count) + ch;
+        }
+        return a;
+    }
+};
