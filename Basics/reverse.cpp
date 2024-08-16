4e47ed9c0f0960fa0c958d62f6e6b10df39b5cac
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2024-08-17 01:03:11 +0530

    Create reverse.cpp

---
+++ Basics/reverse.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int reverse(int x) {
+        // int noOfDigits = 0;
+        int reversedNum = 0;
+        while(x != 0) {
+            if(x > 0) {
+                if(reversedNum > (INT_MAX - x) / 10)
+                    return 0;
+            }
+            else if(x < 0) {
+                if(reversedNum < (INT_MIN - x) / 10)
+                    return 0;
+            }
+            reversedNum *= 10;
+            reversedNum += x % 10;
+            x = x/10;
+            // noOfDigits++;
+        }
+        return reversedNum;
+    }
+};
