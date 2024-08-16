e7aad61a6f1afb6e50a19b9d11078c5fff7b1135
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2024-08-17 00:18:10 +0530

    Create compareNM.cpp

---
+++ Basics/compareNM.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    string compareNM(int n, int m){
+        if(n < m)
+            return "lesser";
+        else if(n == m)
+            return "equal";
+        else
+            return "greater";
+    }
+};
