a5c0e7b1fb27c5b6f90c45392ee5ec78a798da63
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2024-08-17 01:05:49 +0530

    Create checkPalindrome.cpp

---
+++ Basics/checkPalindrome.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string str = to_string(x);
+        for(int i = 0, j = str.size() - 1; i < j; i++, j--) {
+            if(str[i] != str[j])
+                return false;
+        }
+        return true;
+    }
+};
