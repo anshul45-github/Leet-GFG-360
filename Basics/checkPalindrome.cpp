f77afee6d3ba379dc9178553a0bc33e4c1c20eef
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2024-08-17 01:10:20 +0530

    Update checkPalindrome.cpp

--- Basics/checkPalindrome.cpp
+++ Basics/checkPalindrome.cpp
@@ -11,4 +11,19 @@ public:
         }
         return true;
     }
+
+    bool isPalindromeWithoutConvertingIntToStr(int x) {
+        if(x < 0)
+            return false;
+        vector<int> digits;
+        while(x > 0) {
+            digits.push_back(x % 10);
+            x /= 10;
+        }
+        for(int i = 0, j = digits.size() - 1; i < j; i++, j--) {
+            if(digits[i] != digits[j])
+                return false;
+        }
+        return true;
+    }
 };
