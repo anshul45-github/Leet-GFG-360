bb3cf964a1ef97ad7a4a752a2fcb0a665c4d948c
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2024-08-17 02:25:05 +0530

    Create GCDorHCF.cpp

---
+++ Basics/GCDorHCF.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int getGCD(long long a, long long b) {
+        if(b == 0)
+            return a;
+        return getGCD(b, a % b);
+    }
+  
+    vector<long long> lcmAndGcd(long long A , long long B) {
+        vector<long long> ans;
+        int GCD = getGCD(max(A, B), min(A, B));
+        ans.push_back(A * B / GCD);
+        ans.push_back(GCD);
+        return ans;
+    }
+};
