ff01a6c39f31a23904ac6759145331f321bdb017
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2024-08-19 14:32:24 +0530

    Create factorialNumbersRecursive.cpp

---
+++ Basics/factorialNumbersRecursive.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    void helper(long long n, int i, long long k, vector<long long>& nums) {
+        if(k > n)
+            return;
+        nums.push_back(k);
+        helper(n, i + 1, k * i, nums);
+        return;
+    }
+  
+    vector<long long> factorialNumbers(long long n) {
+        int i = 2;
+        long long k = 1;
+        vector<long long> nums;
+        helper(n, i, k, nums);
+        return nums;
+    }
+};
