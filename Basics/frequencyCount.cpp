2f6eeac3e352ed31ee91e5b17a6bb428e126ed17
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2024-08-22 00:31:09 +0530

    Create frequencyCount.cpp

---
+++ Basics/frequencyCount.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void frequencyCount(vector<int>& arr, int N, int P) {
+        int* a = new int[N];
+        for(int i = 0; i < N; i++)
+            arr[i] = 0;
+        for(int i = 0; i < N; i++) {
+            if(arr[i] > N)
+                continue;
+            a[arr[i] - 1]++;
+        }
+        for(int i = 0; i < N; i++) {
+            arr[i] = a[i];
+        }
+        return;
+    }
+};
