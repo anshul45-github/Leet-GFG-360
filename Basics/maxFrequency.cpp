11a4b4f4294d33bb5644272bb95e09db43cb506d
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2024-08-22 22:32:00 +0530

    Create maxFrequency.cpp

---
+++ Basics/maxFrequency.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k) {
+        long long i = 0, j = 1, maxlen = 1, needed = 0, K = k;
+        sort(nums.begin(), nums.end());
+        while(j < nums.size()) {
+            needed += (j - i) * (nums[j] - nums[j - 1]);
+            while(needed > K && i < j) {
+                needed -= nums[j] - nums[i];
+                i++;
+            }
+            maxlen = max(maxlen, j - i + 1);
+            j++;
+        }
+        return maxlen;
+    }
+};
