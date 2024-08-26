57d4c4a302a8364362f5bb5712ffd24aa3588e20
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2024-08-27 00:39:51 +0530

    Create recursiveInsertionSort.cpp

---
+++ Sorting-methods/recursiveInsertionSort.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+    private:
+    void helper(int arr[], int i, int n)
+    {
+        if(i == n)
+            return;
+        int key = arr[i];
+        int j = i - 1;
+        while(j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+        helper(arr, i + 1, n);
+    }
+    public:
+    void insertionSort(int arr[], int n)
+    {
+        helper(arr, 1, n);
+    }
+};
