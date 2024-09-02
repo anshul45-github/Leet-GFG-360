9db58fc9e0b66576a7acf688a915d4f037576420
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2024-09-02 22:16:35 +0530

    Create quickSort.cpp

---
+++ Sorting-methods/quickSort.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+    public:
+    //Function to sort an array using quick sort algorithm.
+    void quickSort(int arr[], int low, int high)
+    {
+        if(low >= high)
+            return;
+        int p = partition(arr, low, high);
+        quickSort(arr, low, p - 1);
+        quickSort(arr, p + 1, high);
+    }
+    
+    public:
+    int partition (int arr[], int low, int high)
+    {
+        int key = arr[high];
+        int j = high - 1;
+        int i = 0;
+        while(i <= j) {
+            if(arr[i] > key) {
+                // swap(arr[j], arr[j - 1]);
+                swap(arr[i], arr[j]);
+                j--;
+            }
+            else
+                i++;
+        }
+        swap(arr[j + 1], arr[high]);
+        // arr[j] = key;
+        return j + 1;
+    }
+};
