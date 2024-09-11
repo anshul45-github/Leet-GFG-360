da00b32832f5f6d4df16f5e2f783f81933ebdbc3
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2024-09-12 00:52:17 +0530

    Create majorityElement.cpp

---
+++ Arrays/majorityElement.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count = 1;
+        int candidate = nums[0];
+        for(int i = 1; i < nums.size(); i++) {
+            if(count == 0)
+                candidate = nums[i];
+            if(nums[i] == candidate)
+                count++;
+            else
+                count--;
+        }
+        return candidate;
+    }
+};
