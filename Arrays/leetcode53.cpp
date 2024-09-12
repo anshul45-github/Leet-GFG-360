d4cffbc8f7979a5285b9ccbfb27b3b3c567f9a88
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2024-09-12 17:49:33 +0530

    Create leetcode53.cpp

---
+++ Arrays/leetcode53.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum = INT_MIN;
+        int currSum = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            currSum = max(currSum + nums[i], nums[i]);
+            maxSum = max(maxSum, currSum);
+        }
+        return maxSum;
+    }
+};
