1e7a19788caa92743421670f7132436300a0492f
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2024-09-15 09:06:18 +0530

    Create leetcode560.cpp

---
+++ Arrays/leetcode560.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        map<int, int> sums;
+        int n = nums.size();
+        int sum = 0;
+        int subarrays = 0;
+        for(int i = 0; i < n; i++) {
+            sum += nums[i];
+            if(sum == k)
+                subarrays++;
+            if(sums[sum - k])
+                subarrays += sums[sum - k];    
+            sums[sum]++;       
+        }
+        return subarrays;
+    }
+};
