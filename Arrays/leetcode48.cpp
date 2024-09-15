3f2796d7546d4720c3675d889b16143ca4331f08
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2024-09-15 07:12:52 +0530

    Create leetcode48.cpp

---
+++ Arrays/leetcode48.cpp
@@ -0,0 +1,24 @@
+// In-place
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for(int i = 0; i < n; i++) {
+            for(int j = i; j < n; j++) {
+                int k = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = k;
+            }
+        }
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n / 2; j++) {
+                int k = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = k;
+            }
+        }
+    }
+};
