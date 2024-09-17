34d90f5f648963f21ff7ed48d1d1fbe4edd4a838
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2024-09-17 19:27:30 +0530

    Create 3sum.cpp

---
+++ Arrays/3sum.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < n - 2; i++) {
+            if(i > 0 && nums[i] == nums[i - 1])
+                continue;
+            int j = i + 1;
+            int k = n - 1;
+            while(j < k) {
+                if(nums[i] + nums[j] + nums[k] == 0) {
+                    vector<int> a = {nums[i], nums[j], nums[k]};
+                    ans.push_back(a);
+                    j++;
+                    k--;
+                    while (nums[j] == nums[j-1] && j < k) {
+                        j++;
+                    }
+                }
+                else if(nums[i] + nums[j] + nums[k] < 0) {
+                    j++;
+                }
+                else {
+                    k--;
+                }
+            }
+        }
+        return ans;
+    }
+};
