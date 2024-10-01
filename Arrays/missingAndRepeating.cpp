9eafcdf73f09142eb7be4003f048c78e655356fd
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2024-10-02 03:37:18 +0530

    Create missingAndRepeating.cpp

---
+++ Arrays/missingAndRepeating.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int maxLen(vector<int>& arr, int n) {
+        int maxLen = 0;
+        int currSum = 0;
+        unordered_map<int, int> mp;
+        for(int i = 0; i < n; i++) {
+            currSum += arr[i];
+            if(!mp[currSum])
+                mp[currSum] = i + 1;
+            if(currSum == 0)
+                maxLen = max(maxLen, i + 1);
+            if(mp[currSum])
+                maxLen = max(maxLen, i - mp[currSum] + 1);
+        }
+        return maxLen;
+    }
+};
