58ed65245c97f302a7cf6ea14406290fa05b5746
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2024-10-06 01:12:31 +0530

    Create ceilTheFloor.cpp

---
+++ Binary Search/ceilTheFloor.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    vector<int> getFloorAndCeil(int x, vector<int> &arr) {
+        vector<int> v = {INT_MIN, INT_MAX};
+        for(int i = 0; i < arr.size(); i++) {
+            if(arr[i] <= x) {
+                v[0] = max(v[0], arr[i]);
+            }
+            if(arr[i] >= x) {
+                v[1] = min(v[1], arr[i]);
+            }
+        }
+        if(v[0] == INT_MIN)
+            v[0] = -1;
+        if(v[1] == INT_MAX)
+            v[1] = -1;
+        return v;
+    }
+};
