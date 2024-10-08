95ce4b75caaceee766da308bc70e1d0420ad4bdb
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2024-10-08 18:35:50 +0530

    Create leetcode410.cpp

---
+++ Binary Search/leetcode410.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    bool helper(vector<int> nums, int maxSum, int n, int k) {
+        int sum = 0;
+        int noOfSubarrays = 0;
+        for(int i = 0; i < n; i++) {
+            if(nums[i] > maxSum)
+                return false;
+            sum += nums[i];
+            if(sum > maxSum) {
+                noOfSubarrays++;
+                sum = nums[i];
+            }
+        }
+        return noOfSubarrays <= k - 1;
+    }
+public:
+    int splitArray(vector<int>& nums, int k) {
+        int n = nums.size();
+        int sum = 0;
+        for(int i = 0; i < n; i++)
+            sum += nums[i];
+        int l = 0;
+        int h = sum;
+        while(l <= h) {
+            int mid = l + ((h - l) >> 1);
+            if(helper(nums, mid, n, k)) {
+                h = mid - 1;
+            }
+            else {
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+};
