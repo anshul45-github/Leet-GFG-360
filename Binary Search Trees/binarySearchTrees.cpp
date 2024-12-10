5487dd7f28b8d95125107e1cfb4c1cb42cd70b7b
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2024-12-10 12:36:30 +0530

    Create binarySearchTrees.cpp

---
+++ Binary Search Trees/binarySearchTrees.cpp
@@ -0,0 +1,14 @@
+// TC : O(n), SC : O(1)
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    bool isBSTTraversal(vector<int>& arr) {
+        for(int i = 1; i < arr.size(); i++) {
+            if(arr[i] <= arr[i - 1])
+                return false;
+        }
+        return true;
+    }
+};
