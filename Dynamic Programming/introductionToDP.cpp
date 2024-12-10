fe09581d6430cb8f697d8b6497b4768fa72f15c2
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2024-12-10 21:49:47 +0530

    Create introductionToDP.cpp

---
+++ Dynamic Programming/introductionToDP.cpp
@@ -0,0 +1,30 @@
+// TC : O(N), SC : O(N)
+#include<bits/stdc++.h>
+using namespace std;
+
+#define N 1000000007
+
+class Solution {
+    long long int solveTopDown(int n, vector<long long int>& dp) {
+        if(dp[n] != -1)
+            return dp[n];
+        dp[n] = (solveTopDown(n - 1, dp) + solveTopDown(n - 2, dp)) % N;
+        return dp[n];
+    }
+  public:
+    long long int topDown(int n) {
+        vector<long long int> dp(n + 1, -1);
+        dp[0] = 0;
+        dp[1] = 1;
+        return solveTopDown(n, dp);
+    }
+    long long int bottomUp(int n) {
+        vector<long long int> dp(n + 1, -1);
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % N;
+        }
+        return dp[n];
+    }
+};
