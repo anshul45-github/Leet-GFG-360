8058a1e9c1417c0fac30c34dcf48d162e7f41ebf
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2024-12-12 13:05:25 +0530

    Create countLinkedListNodes.cpp

---
+++ Linked List/countLinkedListNodes.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    // Function to count nodes of a linked list.
+    int getCount(struct Node* head) {
+
+        int count = 0;
+        Node* temp = head;
+        while(temp) {
+            count++;
+            temp = temp -> next;
+        }
+        return count;
+    }
+};
