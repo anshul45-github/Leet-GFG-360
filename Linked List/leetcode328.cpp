424707f7acee8d5c433370c63bb54948273763c8
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2024-12-14 14:16:10 +0530

    Create leetcode328.cpp

---
+++ Linked List/leetcode328.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(!head) {
+            return nullptr;
+        }
+        ListNode* odd = head;
+        ListNode* even = head -> next;
+        ListNode* o = odd;
+        ListNode* e = even;
+        while(odd && odd -> next && even && even -> next) {
+            odd -> next = odd -> next -> next;
+            even -> next = even -> next -> next;
+            odd = odd -> next;
+            even = even -> next;
+        }
+        odd -> next = e;
+        return o;
+    }
+};
