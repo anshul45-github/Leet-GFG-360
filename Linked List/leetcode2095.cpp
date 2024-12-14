c40e6f9c4be84a0d95b954da56d3605d62f9d939
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2024-12-14 16:40:58 +0530

    Create leetcode2095.cpp

---
+++ Linked List/leetcode2095.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        if(!(head -> next)) {
+            return nullptr;
+        }
+        ListNode* slow = head;
+        ListNode* fast = head -> next -> next;
+        while(fast && fast -> next) {
+            slow = slow -> next;
+            fast = fast -> next -> next;
+        }
+        ListNode* t = slow -> next;
+        slow -> next = slow -> next -> next;
+        delete t;
+        return head;
+    }
+};
