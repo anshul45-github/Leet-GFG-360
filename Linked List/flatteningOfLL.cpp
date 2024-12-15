31b0b1c8f7c77da5c28e0f8c70a7820af6eba382
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2024-12-15 21:03:56 +0530

    Create flatteningOfLL.cpp

---
+++ Linked List/flatteningOfLL.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    Node *flatten(Node *root) {
+        vector<Node*> v;
+        Node* horLevel = root;
+        int n = 0;
+        while(horLevel) {
+            v.push_back(horLevel);
+            horLevel = horLevel -> next;
+            n++;
+        }
+        Node* flattenedList = new Node(-1);
+        Node* curr = flattenedList;
+        while(n) {
+            int minNode = 0;
+            while(!v[minNode])
+                minNode++;
+            for(int i = 0; i < v.size(); i++) {
+                if(v[i] && (v[i] -> data) < (v[minNode] -> data))
+                    minNode = i;
+            }
+            curr -> bottom = new Node(v[minNode] -> data);
+            curr = curr -> bottom;
+            v[minNode] = v[minNode] -> bottom;
+            if(!v[minNode])
+                n--;
+        }
+        curr = flattenedList;
+        flattenedList = flattenedList -> bottom;
+        delete curr;
+        return flattenedList;
+    }
+};
