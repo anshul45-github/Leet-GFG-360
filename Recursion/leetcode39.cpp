b93227639d44ab0c5c6370366aab653dcfef410b
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2024-12-17 23:47:34 +0530

    Create leetcode39.cpp

---
+++ Recursion/leetcode39.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    void combinations(vector<vector<int>>& combn, int sum, int i, int target, vector<int>& a, vector<int> curr) {
+        if(sum > target || i == a.size())
+            return;
+        if(sum == target) {
+            combn.push_back(curr);
+            return;
+        }
+        curr.push_back(a[i]);
+        combinations(combn, sum + a[i], i, target, a, curr);
+        curr.pop_back();
+        combinations(combn, sum, i + 1, target, a, curr);
+    }
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> v;
+        vector<int> a;
+        combinations(v, 0, 0, target, candidates, a);
+        return v;
+    }
+};
