cf3bb15f112408e1c0b11cbf41d9517a80ef9277
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2024-12-18 11:35:49 +0530

    Create subsetSums.cpp

---
+++ Recursion/subsetSums.cpp
@@ -0,0 +1,20 @@
+#include<bits/std++.h>
+using namespace std;
+
+class Solution {
+    void subsets_helper(vector<int>& v, int i, vector<int>& nums, int sum) {
+        if(i == nums.size()) {
+            v.push_back(sum);
+            return;
+        }
+        subsets_helper(v, i + 1, nums, sum + nums[i]);
+        subsets_helper(v, i + 1, nums, sum);
+        return;
+    }
+  public:
+    vector<int> subsetSums(vector<int>& arr) {
+        vector<int> sums;
+        subsets_helper(sums, 0, arr, 0);
+        return sums;
+    }
+};
