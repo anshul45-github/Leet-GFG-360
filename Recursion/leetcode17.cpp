376c3164cf590c830f590af6c5e986088132215e
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2024-12-18 19:58:09 +0530

    Create leetcode17.cpp

---
+++ Recursion/leetcode17.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+unordered_map<int, string> mp = {{2, "abc"}, {3, "def"}, {4, "ghi"}, {5, "jkl"}, {6, "mno"}, {7, "pqrs"}, {8, "tuv"}, {9, "wxyz"}};
+
+class Solution {
+    void helper(vector<string>& combns, string digits, int idx, string comb) {
+        if(idx == digits.size()) {
+            if(comb != "")
+                combns.push_back(comb);
+            return;
+        }
+        int curr = digits[idx] - '0';
+        for(int i = 0; i < mp[curr].size(); i++) {
+            helper(combns, digits, idx + 1, comb + mp[curr][i]);
+        }
+    }
+public:
+    vector<string> letterCombinations(string digits) {
+        vector<string> v;
+        helper(v, digits, 0, "");
+        return v;
+    }
+};
