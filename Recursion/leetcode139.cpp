6890d68f01997f2f50632fa149036dd504c4a58f
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2024-12-19 20:47:05 +0530

    Create leetcode139.cpp

---
+++ Recursion/leetcode139.cpp
@@ -0,0 +1,20 @@
+// Using recursion, but gives TLE, as we need dp to solve this.
+class Solution {
+    bool canBrk(int start, string& s, unordered_set<string>& wordDict) {
+        int n = s.size();
+        if(start == n) return 1;
+        string sub;
+        for(int i = start; i < n; i++) {
+            if(wordDict.count(sub += s[i]) && canBrk(i + 1, s, wordDict)) 
+                return 1;
+        }
+        return 0;
+    }
+
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        // Put all words into an unordered_set
+        unordered_set<string> set(wordDict.begin(), wordDict.end());
+        return canBrk(0, s, set);
+    }
+};
