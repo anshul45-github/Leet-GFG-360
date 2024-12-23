afc21ce61417ddca04a62a78f91bb97b65ff145c
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2024-12-23 16:18:16 +0530

    Create postfixToPrefixConversion.cpp

---
+++ Stack and Queues/postfixToPrefixConversion.cpp
@@ -0,0 +1,24 @@
+// TC : O(N), SC : O(N)
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    string postToPre(string post_exp) {
+        stack<string> st;
+        for(int i = 0; i < post_exp.size(); i++) {
+            char ch = post_exp[i];
+            if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9')) {
+                st.push(string (1, ch));
+            }
+            else {
+                string b = st.top();
+                st.pop();
+                string a = st.top();
+                st.pop();
+                st.push(string(1, ch) + a + b);
+            }
+        }
+        return st.top();
+    }
+};
