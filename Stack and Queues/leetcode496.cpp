9e4a5e4d7f069ff55bc1dc14bf18989694879199
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2024-12-24 16:24:59 +0530

    Create leetcode496.cpp

---
+++ Stack and Queues/leetcode496.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> greatest;
+        stack<int> st;
+        for(int i = nums2.size() - 1; i >= 0; i--) {
+            while(!st.empty()) {
+                if(st.top() > nums2[i]) {
+                    greatest[nums2[i]] = st.top();
+                    st.push(nums2[i]);
+                    break;
+                }
+                st.pop();
+            }
+            if(st.empty()) {
+                greatest[nums2[i]] = -1;
+                st.push(nums2[i]);
+            }
+        }
+        for(int i = 0; i < nums1.size(); i++) {
+            nums1[i] = greatest[nums1[i]];
+        }
+        return nums1;
+    }
+};
