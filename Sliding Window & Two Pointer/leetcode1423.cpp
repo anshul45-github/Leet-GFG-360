d14e1e14d862df6fe12a531559aece5c37ab70c8
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2024-12-27 01:14:58 +0530

    Create leetcode1423.cpp

---
+++ Sliding Window & Two Pointer/leetcode1423.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxScore(vector<int>& cardPoints, int k) {
+        int score = 0;
+        for(int i = 0; i < k; i++)
+            score += cardPoints[i];
+        int maxScore = score;
+        for(int i = 0; i < k; i++) {
+            score = score + cardPoints[cardPoints.size() - i - 1] - cardPoints[k - i - 1];
+            maxScore = max(maxScore, score);
+        }
+        return maxScore;
+    }
+};
