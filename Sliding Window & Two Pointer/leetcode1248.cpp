27e741d2bd68843da5313f973d3a63ed6386c76f
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2024-12-27 00:45:50 +0530

    Create leetcode1248.cpp

---
+++ Sliding Window & Two Pointer/leetcode1248.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    int atMost(vector<int>& nums, int k) {
+        int oddVal = 0;
+        int start = 0;
+        int ans = 0;
+        for(int end = 0; end < nums.size(); end++) {
+            if(nums[end] & 1)
+                oddVal++;
+            while(oddVal > k) {
+                if(nums[start] & 1)
+                    oddVal--;
+                start++;
+            }
+            ans += end - start + 1;
+        }
+        return ans;
+    }
+public:
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        return atMost(nums, k) - atMost(nums, k - 1);
+    }
+};
