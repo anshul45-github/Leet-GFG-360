134e99e16f5ea49bbbaa8defd629cc40b6947feb
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2024-12-26 15:22:24 +0530

    Create leetcode3.cpp

---
+++ Sliding Window & Two Pointer/leetcode3.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        if(s.size() == 0)
+            return 0;
+        unordered_map<char, bool> mp;
+        int start = 0;
+        int end = 0;
+        int maxLen = 1;
+        mp[s[0]] = true;
+        for(int i = 1; i < s.size(); i++) {
+            end++;
+            if(mp[s[i]]) {
+                maxLen = max(maxLen, end - start);
+                cout << i << " " << maxLen << endl;
+                while(s[start] != s[i]) {
+                    mp[s[start]] = false;
+                    start++;
+                }
+                start++;
+            }
+            else {
+                mp[s[end]] = true;
+            }
+        }
+        cout << start << endl;
+        maxLen = max(end - start + 1, maxLen);
+        return maxLen;
+    }
+};
