711590d8ced0c1e6b94de55789382e4e51cf5b49
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2024-12-27 13:29:33 +0530

    Create leetcode992.cpp

---
+++ Sliding Window & Two Pointer/leetcode992.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    int atMost(vector<int>& nums, int k) {
+        if(k == 0)
+            return 0;
+        int ans = 0;
+        int start = 0;
+        unordered_map<int, int> mp;
+        for(int i = 0; i < nums.size(); i++) {
+            mp[nums[i]]++;
+            while(mp.size() > k) {
+                mp[nums[start]]--;
+                if(!mp[nums[start]]) {
+                    mp.erase(nums[start]);
+                }
+                start++;
+            }
+            ans += i - start + 1;
+        }
+        return ans;
+    }
+public:
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        return atMost(nums, k) - atMost(nums, k - 1);
+    }
+};
