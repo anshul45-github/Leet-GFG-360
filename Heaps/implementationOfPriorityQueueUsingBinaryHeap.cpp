87ee1006e934b528d0e484e33441cb0dbaae27f3
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2024-12-28 16:48:19 +0530

    Create implementationOfPriorityQueueUsingBinaryHeap.cpp

---
+++ Heaps/implementationOfPriorityQueueUsingBinaryHeap.cpp
@@ -0,0 +1,29 @@
+// TC : O(logn), SC : O(n)
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    void heapify(int H[], int s, int i) {
+        int largest = i;
+        int left = leftChild(i);
+        int right = rightChild(i);
+        if(left <= s && H[largest] < H[left])
+            largest = left;
+        if(right <= s && H[largest] < H[right])
+            largest = right;
+        if(largest != i) {
+            swap(H[largest], H[i]);
+            heapify(H, s, largest);
+        }
+    }
+  public:
+    int extractMax() {
+        int max = H[0];
+        swap(H[0], H[s]);
+        s--;
+        
+        heapify(H, s, 0);
+        
+        return max;
+    }
+};
