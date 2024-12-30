909f87e360bbc7e2399ae8737e12513e428d263c
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2024-12-31 01:43:54 +0530

    Create maximumSumCombinations.cpp

---
+++ Heaps/maximumSumCombinations.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> Solution::solve(vector<int> &A, vector<int> &B, int C) {
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end());
+    int n = A.size();
+    priority_queue<pair<int, pair<int, int>>> pq;
+    for(int i = 0; i < n; i++) {
+        pq.push({A[i] + B[n - 1], {i, n - 1}});
+    }
+    vector<int> ans;
+    while(C--) {
+        auto topNode = pq.top();
+        pq.pop();
+        int sum = topNode.first;
+        int x = topNode.second.first;
+        int y = topNode.second.second;
+        ans.push_back(sum);
+        pq.push({A[x] + B[y - 1], {x, y - 1}});
+    }
+    return ans;
+}
