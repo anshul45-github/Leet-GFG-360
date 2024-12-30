47bcb786854f02afa5192bfd23838241dd1c5a09
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2024-12-31 02:20:42 +0530

    Create leetcode295.cpp

---
+++ Heaps/leetcode295.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class MedianFinder {
+    priority_queue<long> small, large;
+public:
+    MedianFinder() {}
+    
+    void addNum(int num) {
+        small.push(num);
+        large.push(-small.top());
+        small.pop();
+        if (small.size() < large.size()) {
+            small.push(-large.top());
+            large.pop();
+        }
+    }
+    
+    double findMedian() {
+        if(small.size() == large.size())
+            return (small.top() - large.top()) / 2.0;
+        return small.top();
+    }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
