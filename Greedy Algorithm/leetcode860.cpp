2cd2de7f68136b1f2b8aa3640873759ed989ecc1
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2025-01-01 01:54:54 +0530

    Create leetcode860.cpp

---
+++ Greedy Algorithm/leetcode860.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int fives = 0;
+        int tens = 0;
+        for(int i = 0; i < bills.size(); i++) {
+            if(bills[i] == 5)
+                fives++;
+            else if(bills[i] == 10) {
+                tens++;
+                if(fives == 0)
+                    return false;
+                fives--;
+            }
+            else {
+                if(tens && fives) {
+                    tens--;
+                    fives--;
+                }
+                else if(fives >= 3) {
+                    fives -= 3;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
