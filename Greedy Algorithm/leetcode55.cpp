f333a394bd43c4a150ea9f49feb83eb8e527d8fe
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2025-01-01 11:39:57 +0530

    Create leetcode55.cpp

---
+++ Greedy Algorithm/leetcode55.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int i = 0;
+        for(int reach = 0; i < n && i <= reach; i++)
+            reach = max(i + nums[i], reach);
+
+        return i == n;
+    }
+};
