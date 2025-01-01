9ef716c001a2a9c2ec6356cbffa45e252cab1b6d
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2025-01-02 03:43:37 +0530

    Create leetcode435.cpp

---
+++ Greedy Algorithm/leetcode435.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    static bool cmp(vector<int>& p1, vector<int>& p2) {
+        return p1[1] < p2[1];
+    }
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        sort(intervals.begin(), intervals.end(), cmp);
+        int prev = 0;
+        int cnt = 1;
+        for(int i = 1; i < intervals.size(); i++) {
+            if(intervals[i][0] >= intervals[prev][1]) {
+                prev = i;
+                cnt++;
+            }
+        }
+        return n - cnt;
+    }
+};
