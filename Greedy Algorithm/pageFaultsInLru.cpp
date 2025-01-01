258877b076a1d1d0643f85123327f0193fd721b4
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2025-01-02 02:01:12 +0530

    Create pageFaultsInLru.cpp

---
+++ Greedy Algorithm/pageFaultsInLru.cpp
@@ -0,0 +1,26 @@
+// TC : O(N * C), SC : O(C)
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public:
+    int pageFaults(int N, int C, int pages[]){
+        deque<int> dq;
+        int faults = 0;
+        for(int i = 0; i < N; i++) {
+            deque<int>::iterator it;
+            it = find(dq.begin(), dq.end(), pages[i]);
+            if(it == dq.end()) {
+                if(dq.size() == C)
+                    dq.pop_front();
+                dq.push_back(pages[i]);
+                faults++;
+            }
+            else {
+                dq.erase(it);
+                dq.push_back(pages[i]);
+            }
+        }
+        return faults;
+    }
+};
