a80897f55e5bb22a71561c261b69038ebb456e67
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2025-01-02 00:34:14 +0530

    Create shortestJobFirst.cpp

---
+++ Greedy Algorithm/shortestJobFirst.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    long long solve(vector<int>& bt) {
+        sort(bt.begin(), bt.end());
+        int temp = 0;
+        int ans = 0;
+        for(int i = 0; i < bt.size(); i++) {
+            ans += temp;
+            temp += bt[i];
+        }
+        return ans / bt.size();
+    }
+};
