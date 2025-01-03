4da602831e9bb6225df7cdc394aaaa69678acb92
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2025-01-03 13:02:56 +0530

    Create leetcode110.cpp

---
+++ Binary Trees/leetcode110.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+*/
+
+class Solution {
+    int maxDepth(TreeNode* root) {
+        if(root == nullptr)
+            return 0;
+        return 1 + max(maxDepth(root -> left), maxDepth(root -> right));
+    }
+public:
+    bool isBalanced(TreeNode* root) {
+        if(root == nullptr)
+            return true;
+        return isBalanced(root -> left) && isBalanced(root -> right) && (abs(maxDepth(root -> left) - maxDepth(root -> right)) <= 1);
+    }
+};
