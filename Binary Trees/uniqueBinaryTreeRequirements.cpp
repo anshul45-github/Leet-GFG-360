f6e4b116ef44fc96f0ecee819379a26441b46ef6
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2025-01-05 00:38:46 +0530

    Update uniqueBinaryTreeRequirements.cpp

--- Binary Trees/uniqueBinaryTreeRequirements.cpp
+++ Binary Trees/uniqueBinaryTreeRequirements.cpp
@@ -1,12 +1,10 @@
+// TC : O(1), SC : O(1)
 #include<bits/stdc++.h>
 using namespace std;
 
-//User function Template for C++
-
 class Solution {
 public:
-    bool isPossible(int a,int b)
-    {
+    bool isPossible(int a,int b) {
         if(a + b == 3 || a + b == 5)
             return 1;
         return 0;
