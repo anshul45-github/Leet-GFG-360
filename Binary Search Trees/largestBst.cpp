133ecb625fb34dcd176a3720500792b1f37636a9
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2025-01-05 20:20:04 +0530

    Create largestBst.cpp

---
+++ Binary Search Trees/largestBst.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/* 
+  Tree node structure  used in the program
+  struct Node {
+      int data;
+      Node *left;
+      Node *right;
+
+      Node(int val) {
+          data = val;
+          left = right = NULL;
+      }
+  };
+*/
+
+class Solution{
+    vector<int> helper(Node* root) {
+        if(root == nullptr) {
+            return {0, INT_MAX, INT_MIN};
+        }
+        vector<int> left = helper(root -> left);
+        vector<int> right = helper(root -> right);
+        if(root -> data > left[2] && root -> data < right[1])
+            return {1 + left[0] + right[0], min(root -> data, left[1]), max(root -> data, right[2])};
+        else
+            return {max(left[0], right[0]), INT_MIN, INT_MAX};
+    }
+    public:
+    int largestBst(Node *root)
+    {
+        return helper(root)[0];
+    }
+};
