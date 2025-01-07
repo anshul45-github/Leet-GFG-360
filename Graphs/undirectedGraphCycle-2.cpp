5b687d3e5c8f39709f5d5ac35c3ec4621483c2ea
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2025-01-07 14:37:49 +0530

    Create undirectedGraphCycle-2.cpp

---
+++ Graphs/undirectedGraphCycle-2.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    bool dfsTraversal(vector<vector<int>>& adj, vector<bool>& vis, int curr, int parent) {
+        vis[curr] = true;
+        for(int i = 0; i < adj[curr].size(); i++) {
+            if(adj[curr][i] == parent) {
+                continue;
+            }
+            else if(vis[adj[curr][i]])
+                return true;
+            else {
+                if(dfsTraversal(adj, vis, adj[curr][i], curr))
+                    return true;
+            }
+        }
+        return false;
+    }
+  public:
+    // Function to detect cycle in an undirected graph.
+    bool isCycle(vector<vector<int>>& adj) {
+        vector<bool> vis(adj.size(), false);
+        for(int i = 0; i < adj.size(); i++) {
+            if(!vis[i]) {
+                if(dfsTraversal(adj, vis, i, -1) == true)
+                    return true;
+            }
+        }
+        return false;
+    }
+};
