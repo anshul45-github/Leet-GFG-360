22229aa5209d9bb1538450b8b900b15661a4944d
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2025-01-08 03:36:37 +0530

    Create leetcode213.cpp

---
+++ Dynamic Programming/leetcode213.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    int solve(vector<int>& nums, int i, int n, vector<int>& dp) {
+        if(i >= n)
+            return 0;
+        if(dp[i] != -1)
+            return dp[i];
+        int include = solve(nums, i + 2, n, dp) + nums[i];
+        int exclude = solve(nums, i + 1, n, dp);
+        return dp[i] = max(include, exclude);
+    }
+public:
+    int rob(vector<int>& nums) {
+        if(nums.size() == 1)
+            return nums[0];
+        vector<int> dp1(nums.size(), -1);
+        vector<int> dp2(nums.size(), -1);
+        return max(solve(nums, 0, nums.size() - 1, dp1), solve(nums, 1, nums.size(), dp2));
+    }
+};
