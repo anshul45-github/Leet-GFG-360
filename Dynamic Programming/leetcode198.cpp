b4041e33d356daaf6b7d6c31b4e2db0f6afb5acc
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2025-01-08 03:32:10 +0530

    Create leetcode198.cpp

---
+++ Dynamic Programming/leetcode198.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    int solve(vector<int>& nums, int i, vector<int>& dp) {
+        if(i >= nums.size())
+            return 0;
+        if(dp[i] != -1)
+            return dp[i];
+        int include = solve(nums, i + 2, dp) + nums[i];
+        int exclude = solve(nums, i + 1, dp);
+        return dp[i] = max(include, exclude);
+    }
+public:
+    int rob(vector<int>& nums) {
+        vector<int> dp(nums.size(), -1);
+        int ans = solve(nums, 0, dp);
+        return ans;
+    }
+};
