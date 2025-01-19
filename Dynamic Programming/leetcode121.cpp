217bdd2b50674191bcd515cb1b6bda787a282ac1
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2025-01-19 23:17:25 +0530

    Create leetcode121.cpp

---
+++ Dynamic Programming/leetcode121.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mini = INT_MAX;
+        int maxProfit = 0;
+        for(int i = 0; i < prices.size(); i++) {
+            mini = min(mini, prices[i]);
+            maxProfit = max(maxProfit, prices[i] - mini);
+        }
+        return maxProfit;
+    }
+};
