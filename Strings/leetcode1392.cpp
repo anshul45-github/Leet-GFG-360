f59e06479f52bd8659ddcbf31f6c7d40aa6b1ff7
Anshul Mendiratta <145244762+anshul45-github@users.noreply.github.com>
2025-02-03 00:03:46 +0530

    Create leetcode1392.cpp

---
+++ Strings/leetcode1392.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define N 1000000007
+
+class Solution {
+public:
+    string longestPrefix(string s) {
+        int maxI = -1;
+        long long hash = 0;
+        long long revHash = 0;
+        long long p_curr = 1;
+        for(int i = 0; i < s.size() - 1; i++) {
+            hash = (hash * 31 + s[i]) % N;
+            revHash = (revHash + p_curr * s[s.size() - i - 1]) % N;
+            p_curr = (p_curr * 31) % N;
+            if(hash == revHash)
+                maxI = i;
+        }
+        return s.substr(0, maxI + 1);
+    }
+};
