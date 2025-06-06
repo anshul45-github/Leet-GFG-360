41f6ae9ad71ef1b6f0e1993798f77efb93115cd4
Anshul <anshulmendiratta10d@gmail.com>
2025-06-06 19:42:15 +0530

    refactor: simplify max frequency calculation in characterReplacement method

--- Sliding Window & Two Pointer/Longest_Repeating_Character_Replacement.cpp
+++ Sliding Window & Two Pointer/Longest_Repeating_Character_Replacement.cpp
@@ -6,26 +6,21 @@
 using namespace std;
 
 class Solution {
-    int max(int a, int b) {
-        return a >= b ? a : b;
-    }
 public:
     int characterReplacement(string s, int k) {
         int start = 0;
         int maxAns = 0;
-        unordered_map<char, int> mp;
         int maxFreq = 0;
+        unordered_map<char, int> mp;
         for(int end = 0; end < s.size(); end++) {
             mp[s[end]]++;
-            if(mp[s[end]] > maxFreq)
+            if(maxFreq < mp[s[end]])
                 maxFreq = mp[s[end]];
             while(end - start + 1 - maxFreq > k) {
                 mp[s[start]]--;
                 start++;
-                maxFreq = 0;
-                for(char ch = 'A'; ch <= 'Z'; ch++) {
+                for(char ch = 'A'; ch <= 'Z'; ch++)
                     maxFreq = max(maxFreq, mp[ch]);
-                }
             }
             maxAns = max(maxAns, end - start + 1);
         }
