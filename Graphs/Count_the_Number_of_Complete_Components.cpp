4c7319dbafab6fd86011ad2679b243124b78e0d2
Anshul <anshulmendiratta10d@gmail.com>
2025-06-25 20:17:34 +0530

    feat: add implementation for "Count the Number of Complete Components" problem

---
+++ Graphs/Count_the_Number_of_Complete_Components.cpp
@@ -0,0 +1,41 @@
+// https://leetcode.com/problems/count-the-number-of-complete-components/
+// Medium
+
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+    bool isCompleteConnected(vector<unordered_set<int>>& adj, vector<bool>& vis, int curr) {
+        auto& s = adj[curr];
+        for(int n : s) 
+            vis[n] = true;
+        for(int n : s) {
+            if(adj[n].size() != s.size())
+                return false;
+            for(int x : adj[n]) {
+                if(!s.count(x))
+                    return false;
+            }
+        }
+        return true;
+    }
+public:
+    int countCompleteComponents(int n, vector<vector<int>>& edges) {
+        vector<unordered_set<int>> adj(n);
+        for(auto& edge : edges) {
+            adj[edge[0]].insert(edge[1]);
+            adj[edge[1]].insert(edge[0]);
+        }
+        for(int i = 0; i < n; i++)
+            adj[i].insert(i);
+        vector<bool> vis(n, false);
+        int ans = 0;
+        for(int i = 0; i < n; i++) {
+            if(vis[i])
+                continue;
+            ans += isCompleteConnected(adj, vis, i);
+        }
+        return ans;
+    }
+};
