728bde7c28e42e69158b525ff6d94f9b9f33cd7b
Anshul <anshulmendiratta10d@gmail.com>
2025-07-08 15:15:25 +0530

    Refactored Minimum Size Subarray Sum implementation by consolidating code into a single file and enhancing readability. Removed outdated file.

--- Sliding Window & Two Pointer/Minimum_Size_Subarray_Sum.cpp
+++ Sliding Window & Two Pointer/Minimum_Size_Subarray_Sum.cpp
@@ -1,8 +1,43 @@
-// https://leetcode.com/problems/minimum-size-subarray-sum/description/?envType=problem-list-v2&envId=prefix-sum
+// https://leetcode.com/problems/minimum-size-subarray-sum/
 
 #include<bits/stdc++.h>
 using namespace std;
 
+// Approach 1: Using Prefix Sum and Binary Search
+class Solution {
+    int floor(vector<int>& prefixSum, int target) {
+        int low = 0, high = prefixSum.size() - 1, ans = -1;
+        while(low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if(prefixSum[mid] <= target) {
+                ans = mid;
+                low = mid + 1;
+            }
+            else
+                high = mid - 1;
+        }
+        return ans;
+    }
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        vector<int> prefixSum;
+        int currSum = 0;
+        int ans = INT_MAX;
+        for(int i = 0; i < nums.size(); i++) {
+            int x = nums[i];
+            currSum += x;
+            int floorIdx = floor(prefixSum, currSum - target);
+            if(floorIdx != -1)
+                ans = min(ans, i - floorIdx);
+            else if(currSum >= target)
+                ans = min(ans, i + 1);
+            prefixSum.push_back(currSum);
+        }
+        return ans == INT_MAX ? 0 : ans;
+    }
+};
+
+// Optimized Approach: Using Sliding Window Technique
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
