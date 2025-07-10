9d722d6be41a5b90d72d1f4f98d7210915cf922a
Anshul <anshulmendiratta10d@gmail.com>
2025-07-11 01:50:31 +0530

    Refactored characterReplacement function to improve clarity and performance by replacing unordered_map with a frequency array and simplifying variable names.

--- Sliding Window & Two Pointer/Longest_Repeating_Character_Replacement.cpp
+++ Sliding Window & Two Pointer/Longest_Repeating_Character_Replacement.cpp
@@ -6,20 +6,22 @@ using namespace std;
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int l = 0;
-        int maxlen = 0;
+        int left = 0;
+        vector<int> freq(26, 0);
         int maxFreq = 0;
-        unordered_map<char, int> mp;
-        for(int r = 0; r < s.size(); r++) {
-            maxFreq = max(maxFreq, ++mp[s[r]]);
-            while(r - l + 1 - maxFreq > k) {
-                mp[s[l]]--;
-                l++;
-                for(char ch = 'A'; ch <= 'Z'; ch++)
-                    maxFreq = max(maxFreq, mp[ch]);
+        int ans = 0;
+        for(int right = 0; right < s.size(); right++) {
+            maxFreq = max(maxFreq, ++freq[s[right] - 'A']);
+            while(right - left + 1 - maxFreq > k) {
+                freq[s[left] - 'A']--;
+                int maxi = 0;
+                for(int x : freq)
+                    maxi = max(maxi, x);
+                maxFreq = maxi;
+                left++;
             }
-            maxlen = max(maxlen, r - l + 1);
+            ans = max(ans, right - left + 1);
         }
-        return maxlen;
+        return ans;
     }
 };
