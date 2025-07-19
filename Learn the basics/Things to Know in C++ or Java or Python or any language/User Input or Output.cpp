c75569309597d50be5b690114b21ed780f78b2ce
Anshul <anshulmendiratta10d@gmail.com>
2025-07-20 02:55:45 +0530

    Add User Input and Output example with multiplication function

---
+++ Learn the basics/Things to Know in C++ or Java or Python or any language/User Input or Output.cpp
@@ -0,0 +1,11 @@
+// https://www.geeksforgeeks.org/problems/c-input-output2432/1
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int multiplication(int A, int B) {
+        return A * B;
+    }
+};
