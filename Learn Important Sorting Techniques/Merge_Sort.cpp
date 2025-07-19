e471c9bb0ff466f2b786bc4cb5478fc90bf1e75d
Anshul <anshulmendiratta10d@gmail.com>
2025-07-20 04:11:15 +0530

    Remove obsolete implementations of Merge Sort, Recursive Bubble Sort, and Recursive Insertion Sort; add updated versions of these sorting algorithms.

--- Learn Important Sorting Techniques/Merge_Sort.cpp
+++ Learn Important Sorting Techniques/Merge_Sort.cpp
@@ -1,3 +1,5 @@
+// https://www.geeksforgeeks.org/problems/merge-sort/1
+
 #include<bits/stdc++.h>
 using namespace std;
 
--- Learn Important Sorting Techniques/Quick_Sort.cpp
+++ Learn Important Sorting Techniques/Quick_Sort.cpp
@@ -1,3 +1,5 @@
+// https://www.geeksforgeeks.org/problems/quick-sort/1
+
 #include<bits/stdc++.h>
 using namespace std;
 
